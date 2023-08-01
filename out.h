5da3980e47e6fcee1a871b7ab69c30fc8d3bf07b
KelTRM <kelvin@keltrm.com>
2023-07-31 19:54:24 -0500

    Add coloring to syntax errors and add file CommandLineParser.h

---
+++ out.h
@@ -0,0 +1,8 @@
+// This file was compiled by the KRCTP.
+// (Kelvin's ResourCe TransPiler)
+#ifndef Item1
+#define Item1 25
+#endif
+#ifndef Item2
+#define Item2 "Hello, World"
+#endif
--- src/Error.h
+++ src/Error.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include"SyntaxError.h"
+#include"TermColors.h"
 
-#define ReportSyntaxError(Error)									\
-					printf("Syntax Error on line %zu: %s\n"			\
-									"Help: %s\n"					\
-									"\"%s\"\n",						\
-									Error.GetLineNumber(),			\
-									Error.GetErr().c_str(),			\
-									Error.GetHelpInfo().c_str(),	\
-									Error.GetLine().c_str())
+#define ReportSyntaxError(Error)											\
+					printf(TERM_COLOR_RED"Syntax Error on line %zu: %s\n"	\
+						   TERM_COLOR_BLUE "Help: %s\n" TERM_COLOR_DEFAULT	\
+						   "\"%s\"\n\n",									\
+						   Error.GetLineNumber(),							\
+						   Error.GetErr().c_str(),							\
+						   Error.GetHelpInfo().c_str(),						\
+						   Error.GetLine().c_str())
